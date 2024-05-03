e4074ea8dbc067be41d6262591493931e3cc2b0a
Esther Carrelle <esthcarelle@gmail.com>
2024-05-03 11:50:27 +0200

    Add a column

--- onboarding/onboarding_presentation/src/main/java/com/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -1,8 +1,11 @@
 package com.onboarding_presentation.welcome
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 
 @Composable
 fun WelcomeScreen() {
 
+    Column(){
+    }
 }
