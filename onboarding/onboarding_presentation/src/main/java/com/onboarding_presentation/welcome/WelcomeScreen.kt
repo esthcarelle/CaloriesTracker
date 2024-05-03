38bbcbdd0f0963078ec3d3ddce3a2272ff91a1ef
Esther Carrelle <esthcarelle@gmail.com>
2024-05-03 11:58:37 +0200

    Style the text

--- onboarding/onboarding_presentation/src/main/java/com/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -3,17 +3,27 @@ package com.onboarding_presentation.welcome
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import com.plcoding.core.R
 
 @Composable
 fun WelcomeScreen() {
 
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = stringResource(id = R.string.))
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.welcome_text),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h1
+        )
     }
 }
