df6a82bcc12ab0c140d9dd521029f644517b5680
Esther Carrelle <esthcarelle@gmail.com>
2024-05-03 11:51:37 +0200

    Arrange the column in center

--- onboarding/onboarding_presentation/src/main/java/com/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -1,5 +1,6 @@
 package com.onboarding_presentation.welcome
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -8,6 +9,6 @@ import androidx.compose.ui.Modifier
 @Composable
 fun WelcomeScreen() {
 
-    Column(modifier = Modifier.fillMaxSize()){
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center){
     }
 }
