19c68a468a08ca7cfcc18e6777f84febfdd0d918
Esther Carrelle <esthcarelle@gmail.com>
2024-05-03 12:07:46 +0200

    Add the core ui module

--- onboarding/onboarding_presentation/src/main/java/com/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -2,7 +2,9 @@ package com.onboarding_presentation.welcome
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -10,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.plcoding.core.R
 
 @Composable
@@ -26,4 +29,6 @@ fun WelcomeScreen() {
             style = MaterialTheme.typography.h1
         )
     }
+
+    Spacer(modifier = Modifier.height(16.dp))
 }
