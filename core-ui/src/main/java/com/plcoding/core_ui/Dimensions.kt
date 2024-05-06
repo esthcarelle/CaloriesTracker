fa83c212b2ed1a89810a7c59085a6c5e85b933c4
Esther Carrelle <esthcarelle@gmail.com>
2024-05-06 16:01:00 +0200

    Added a space small dimension

--- core-ui/src/main/java/com/plcoding/core_ui/Dimensions.kt
+++ core-ui/src/main/java/com/plcoding/core_ui/Dimensions.kt
@@ -1,8 +1,10 @@
 package com.plcoding.core_ui
 
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 data class Dimensions(
-    val default: Dp,
-    val spaceExtraSmall: Dp
+    val default: Dp  = 0.dp,
+    val spaceExtraSmall: Dp = 4.dp,
+    val spaceSmall: Dp = 8.dp
 )
